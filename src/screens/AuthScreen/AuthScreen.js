9127a0d55a948b15c581c854b2c3a7fea0d6add7
Issarapong Poesua <dekablade01@gmail.com>
2018-03-16 15:49:10 +0700

    login handler

--- src/screens/AuthScreen/AuthScreen.js
+++ src/screens/AuthScreen/AuthScreen.js
@@ -1,37 +1,78 @@
 import React, { Component } from 'react';
-import { View, TextInput, StyleSheet, Button } from 'react-native';
+import { View, TextInput, StyleSheet, Button, AsyncStorage } from 'react-native';
 
 import AuthenticationForm from './AuthenticationForm'
-
+import BalanceScreen from '../BalanceScreen/BalanceScreen'
 export default class AuthScreen extends Component {
+
+    componentDidMount() {
+        this.getAPIFromPersistent()
+    }
 
     state = {
         key: "",
         secret: "",
+        isLoggedIn: false
     }
 
+    saveAPI = () => {
+        AsyncStorage.setItem('apiKey', this.state.key)
+        AsyncStorage.setItem('apiSecret', this.state.secret)
+    }
+
+    getAPIFromPersistent = () => {
+        Promise.all([
+            AsyncStorage.getItem('apiKey'),
+            AsyncStorage.getItem('apiSecret')
+        ]).then( (value) => {
+            this.setState({ key: value[0], secret: value[1] })
+            if (value[0].length == 12 && value[1].length == 12) {
+                this.setState({ isLoggedIn: true })
+            }
+        })
+    }
+    
     secretDidChange = (secret) => {
-        alert(secret)
+        this.setState({ secret: secret })
     }
 
     keyDidChange = (key) => {
-        alert(key)
+        this.setState({ key: key })
     }
 
     buttonDidPressHandler = () => {
-
+        if (this.state.key.length != 12 && this.state.secret != 12) {
+            alert("invalid api key or secret")
+            return;
+        }
+        this.saveAPI()
+        this.setState({ isLoggedIn: true })
+        alert("api saved")
     }
 
     render() {
-        return (
-        <View style={ styles.view }> 
-            <AuthenticationForm 
-                didFillKey={ (text) => this.keyDidChange(text) }
-                didFillSecret={ (text) => this.secretDidChange(text) }
-                didSubmitHandler={ this.buttonDidPressHandler  }
-            />
-        </View>
-        )
+        if ( !this.state.isLoggedIn ) {
+            return (
+                <View style={ styles.view }> 
+                    <AuthenticationForm 
+                        didFillKey={ (text) => this.keyDidChange(text) }
+                        didFillSecret={ (text) => this.secretDidChange(text) }
+                        didSubmitHandler={ this.buttonDidPressHandler  }
+                    />
+                </View>
+                )
+        } else {
+            return <BalanceScreen onPressHandler={ this.a }/>
+        }
+    }
+
+    a = () => {
+        this.setState({ 
+            key: "",
+            secret: "",
+            isLoggedIn: false
+         }); 
+         alert("logged out")
     }
 };
 
