a721b802e91ec54f2e202b333071660fc3dfb870
Issarapong Poesua <dekablade01@gmail.com>
2018-03-16 12:50:31 +0700

    auth screen without navigation

--- src/screens/AuthScreen/AuthScreen.js
+++ src/screens/AuthScreen/AuthScreen.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, TextInput, StyleSheet, Button } from 'react-native';
+
+import AuthenticationForm from './AuthenticationForm'
 
 export default class AuthScreen extends Component {
+
+    state = {
+        key: "",
+        secret: "",
+    }
+
+    secretDidChange = (secret) => {
+        alert(secret)
+    }
+
+    keyDidChange = (key) => {
+        alert(key)
+    }
+
+    buttonDidPressHandler = () => {
+
+    }
+
     render() {
         return (
-        <View> 
-            <Text>Auth Screen</Text>
+        <View style={ styles.view }> 
+            <AuthenticationForm 
+                didFillKey={ (text) => this.keyDidChange(text) }
+                didFillSecret={ (text) => this.secretDidChange(text) }
+                didSubmitHandler={ this.buttonDidPressHandler  }
+            />
         </View>
         )
     }
 };
 
+const styles = StyleSheet.create({
+    view: {
+        height: '100%',
+        backgroundColor: '#eee',
+        flexDirection: 'column',
+        padding: 10
+    },
+});
