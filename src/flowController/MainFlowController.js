7c39040ec91edd80e0b87fc4625af0760ebb8323
Issarapong Poesua <dekablade01@gmail.com>
2018-04-10 20:53:17 +0700

    create navigation controller stack

--- src/flowController/MainFlowController.js
+++ src/flowController/MainFlowController.js
@@ -4,42 +4,16 @@ import AuthenticationScreen from '../screens/AuthScreen/AuthenticationScreen';
 import MainTabBarScreen from '../screens/MainTabBarScreen/MainTabBarScreen';
 import { StackNavigator } from 'react-navigation';
 
-// export default class MainFlowController extends React.Component {
-//   isLoggedIn = false;
-//   render() {
-//     return StackNavigator({
-//       AuthenticationScreen: {
-//         screen: AuthenticationScreen,
-//         navigationOptions: {
-//           header: null,
-//         }
-//       },
-//       MainTabBarScreen: {
-//         screen: MainTabBarScreen,
-//       }
-//     }, { initialRouteName: !this.isLoggedIn ? "AuthenticationScreen" : "MainTabBarScreen"  })
-//   }
-// }
-
 const stack = StackNavigator({
   AuthenticationScreen: {
     screen: AuthenticationScreen,
-    navigationOptions: { header: null },
+    navigationOptions: {
+      header: null,
+    },
   },
+  MainTabBarScreen: {
+    screen: MainTabBarScreen,
+  }
 });
 
 export default stack;
-
-// {
-//   auth: {
-//     screen: AuthenticationScreen,
-//   },
-//   tabbar: {
-//     screen: MainTabBarScreen,
-//   },
-// },
-// {
-//   initialRouteName: !this.isLoggedIn
-//     ? 'AuthenticationScreen'
-//     : 'MainTabBarScreen',
-// }
