2a674bc6b7e2d0d403bf7f7b947fce0eb3054652
Issarapong Poesua <dekablade01@gmail.com>
2018-04-12 01:50:51 +0700

    add button in navigation bar

--- src/FlowControllers/MainFlowController.js
+++ src/FlowControllers/MainFlowController.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { View } from 'react-native';
 import AuthenticationScreen from '../Screens/AuthScreen';
 import MainTabBarScreen from '../Screens/MainTabBarScreen';
+import CircularImageButton from '../components/common/CircularImageButton'
 import { StackNavigator } from 'react-navigation';
 
 const stack = StackNavigator({
   MainTabBarScreen: {
     screen: MainTabBarScreen,
     navigationOptions: {
-      headerLeft: null,
-      gesturesEnable: false,
+      headerLeft: <CircularImageButton url={"https://avatars1.githubusercontent.com/u/6483170?s=460&v=4"} onPress={ () => { alert("navigate to account screen")}}/>,
+      title: "Dashboard",
     },
   },
 });
