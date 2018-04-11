928ba549ead6a39fdfa32362af7e17e077d914bb
Issarapong Poesua <dekablade01@gmail.com>
2018-04-11 11:43:08 +0700

    fix native project files

--- android/app/src/main/java/com/bxwithreactnative/MainApplication.java
+++ android/app/src/main/java/com/bxwithreactnative/MainApplication.java
@@ -1,9 +1,6 @@
 package com.bxwithreactnative;
-
 import android.app.Application;
-
 import com.facebook.react.ReactApplication;
-import com.reactnativenavigation.NavigationReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +20,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new NavigationReactPackage()
+          new MainReactPackage()
       );
     }
 
