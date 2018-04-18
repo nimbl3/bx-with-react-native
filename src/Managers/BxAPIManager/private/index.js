7fae5adf01d847242d7b7519d8a68474e3361e34
Issarapong Poesua <dekablade01@gmail.com>
2018-04-18 18:54:21 +0700

    add completion

--- src/Managers/BxAPIManager/private/index.js
+++ src/Managers/BxAPIManager/private/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import BxAPIManager from '../';
 
-const order = (isBuy, amount, completion) => {
-  fetch(BxAPIManager.hostname + BxAPIManager.endPoint.private.order)
-    .then(response => response.json())
-    .then(responseJSON => {});
-};
 
 const buy = (secondaryAmount, completion) => {
   order(true, secondaryAmount, () => {
-      completion()
+    completion();
   });
 };
 
 const sell = (primaryAmount, completion) => {
   order(false, primaryAmount, () => {
-      completion()
+    completion();
   });
 };
 
@@ -24,5 +19,16 @@ const balance = completion => {
     .then(response => response.json())
     .then(responseJSON => {});
 };
+
+
+
+const order = (isBuy, amount, completion) => {
+    fetch(BxAPIManager.hostname + BxAPIManager.endPoint.private.order)
+      .then(response => response.json())
+      .then(responseJSON => {
+        completion();
+      });
+  };
+  
 
 export default { buy, sell, balance };
