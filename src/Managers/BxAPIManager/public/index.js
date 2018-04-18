038f20690574a9d82aff310dca41aeea92ade03b
Issarapong Poesua <dekablade01@gmail.com>
2018-04-18 18:40:37 +0700

    create ticker and wrapper

---
+++ src/Managers/BxAPIManager/public/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import BxAPIManager from '../'
+
+const ticker = (completion) => {
+    fetch(BxAPIManager.hostname + BxAPIManager.endPoint.public.ticker)
+    .then((response) => response.json())
+    .then((responseJSON) => {
+        return tickerMapper(responseJSON)
+    });
+}
+
+const tickerMapper = (json) => {
+    const numberOfKeys = Object.keys(json).length
+    const result = []
+    Object.keys(json).map((key) => {
+        result.push(json[key])
+        if (numberOfKeys == result.length) {
+            return result;
+        }
+    })
+}
+
+export default { ticker }
