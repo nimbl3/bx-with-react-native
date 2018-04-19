b5c99c1de263f693ed292e69616465cb7767d4f6
Issarapong Poesua <dekablade01@gmail.com>
2018-04-19 11:37:35 +0700

    order wrapper

--- src/Managers/BxAPIManager/private/index.js
+++ src/Managers/BxAPIManager/private/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import BxAPIManager from '../';
-
-
-const buy = (secondaryAmount, completion) => {
-  order(true, secondaryAmount, () => {
-    completion();
-  });
-};
-
-const sell = (primaryAmount, completion) => {
-  order(false, primaryAmount, () => {
-    completion();
-  });
-};
+import BxEncryptor from '../../../Helpers/APIEncryptor';
 
 const balance = completion => {
   fetch(BxAPIManager.hostname + BxAPIManager.endPoint.private.balance)
@@ -20,15 +8,46 @@ const balance = completion => {
     .then(responseJSON => {});
 };
 
+const buy = (pairing, secondaryAmount, rate, completion) => {
+  order(true, secondaryAmount, () => {
+    completion();
+  });
+};
 
+const sell = (pairing, primaryAmount, rate, completion) => {
+  order(false, primaryAmount, () => {
+    completion();
+  });
+};
 
-const order = (isBuy, amount, completion) => {
-    fetch(BxAPIManager.hostname + BxAPIManager.endPoint.private.order)
+const order = (pairing, isBuy, amount, rate, completion) => {
+  fetchWithCredential(
+    BxAPIManager.hostname + BxAPIManager.endPoint.private.order,
+    {
+      pairing: pairing,
+      type: isBuy ? 'buy' : 'sell',
+      amount: amount,
+      rate: rate,
+    },
+    completion
+  );
+};
+
+const fetchWithCredential = (url, body, completion) => {
+  BxEncryptor.getQueryCredential(credential => {
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(Object.assign(body, credential)),
+    })
       .then(response => response.json())
       .then(responseJSON => {
-        completion();
+        if (!responseJSON.success) {
+          completion(null);
+          return;
+        }
+        completion(responseJSON);
       });
-  };
-  
+  });
+};
 
 export default { buy, sell, balance };
